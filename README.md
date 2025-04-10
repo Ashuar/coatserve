199faa9e014ac0a43d4a130fc3085bc746e6c49c
ashuar <ashuar@Xconsol-Ashur>
2025-04-10 16:58:52 +0500

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Coatserve
+
+coarserve customaization
+
+#### License
+
+MIT
---
+++ coatserve/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Coatserve",
+			"type": "module",
+			"label": _("Coatserve")
+		}
+	]
