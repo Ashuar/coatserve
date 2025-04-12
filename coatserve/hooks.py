4a7412513022612be256b5263cad088befe998f2
Ashuar <ashuarchughtai@gmail.com>
2025-04-12 15:03:28 +0500

    deleting fields

--- coatserve/hooks.py
+++ coatserve/hooks.py
@@ -221,12 +221,6 @@ fixtures = [
             "Delivery Note Item-total_cost",
             "Delivery Note Item-quantity_ltr_k",
             "Delivery Note-total_qty_ltr_kg",
-            "Sales Invoice Item-pack_size",
-            "Sales Invoice Item-sale_tax_value",
-            "Sales Invoice Item-total_inclusive_sales_tax",
-            "Sales Invoice Item-price_per_lt_or_kg",
-            "Sales Invoice Item-total_cost",
-            "Sales Invoice Item-quantity_ltr_k",
             ]]
         }
     }
